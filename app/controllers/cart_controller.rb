01e38d3d0f7d0fe5de9b650e16168fa1a2eb2800
alexander ames <alexanderthomasames@gmail.com>
2017-01-11 11:37:02 -0700

    create view order view and method

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,8 +1,15 @@
 class CartController < ApplicationController
-  def add_to_cart
-  end
+	def add_to_cart
+	  line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity]
+
+	  line_item.line_item_total = line_item.quantity * line_item.product.price
+	  line_item.save
+
+	  redirect_to root_path
+	end
 
   def view_order
+  	@line_items = LineItem.all
   end
 
   def checkout
