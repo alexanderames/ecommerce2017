15a5c0a896527f0c088c36ad780a19a9f6d0314a
alexander ames <alexanderthomasames@gmail.com>
2017-01-11 11:47:03 -0700

    create checkout method

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,7 @@
 class CartController < ApplicationController
+	
+	before_filter :authenticate_user!, :except => [:add_to_cart, :view_order]
+
 	def add_to_cart
 	  line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity]
 
@@ -12,6 +15,24 @@ class CartController < ApplicationController
   	@line_items = LineItem.all
   end
 
-  def checkout
-  end
+	def checkout
+	  @line_items = LineItem.all
+	  @order = Order.create(user_id: current_user.id, subtotal: 0)
+
+	  @line_items.each do |line_item|
+	    @order.order_items[line_item.product_id] = line_item.quantity
+	    @order.subtotal += line_item.line_item_total
+	  end
+
+	  @order.sales_tax = @order.subtotal * 0.07
+	  @order.grand_total = @order.subtotal + @order.salex_tax
+	  @order.save
+
+	  @line_items.each do |line_item|
+	    line_item.product.quantity -= line_item.quantity
+	    line_item.product.save
+	  end
+
+	  LineItem.destroy_all
+	end
 end
